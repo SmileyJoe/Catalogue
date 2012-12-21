a1f31e5ab1fbd77331425802d0025f446abd2025
Cody Strugnell <cody.strugnell@gmail.com>
2012-12-21 13:00:44 +0200

    Initial Commit

---
+++ src/com/smileyjoedev/catalogue/Main.java
@@ -0,0 +1,25 @@
+package com.smileyjoedev.catalogue;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
+import android.os.Bundle;
+
+public class Main extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+	}
+
+}
